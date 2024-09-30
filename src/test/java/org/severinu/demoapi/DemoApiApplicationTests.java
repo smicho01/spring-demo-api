ff97b9b686264ca5f863e40b71a8bfb2069efb89
seweryn.michota <seweryn.michota@digital.homeoffice.gov.uk>
2024-09-30 12:24:01 +0100

    Add get app people test

--- src/test/java/org/severinu/demoapi/DemoApiApplicationTests.java
+++ src/test/java/org/severinu/demoapi/DemoApiApplicationTests.java
@@ -1,13 +1,26 @@
 package org.severinu.demoapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.severinu.demoapi.api.entity.Person;
+import org.severinu.demoapi.api.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest
 class DemoApiApplicationTests {
 
+    @Autowired
+    private PersonService  personService;
+
     @Test
-    void contextLoads() {
+    void testGetAll_shouldReturnNotEmptyList() {
+        List<Person> all = personService.getAll();
+
+        Assertions.assertFalse(all.isEmpty());
     }
 
 }
---
+++ src/test/java/org/severinu/demoapi/api/service/PersonServiceTest.java
@@ -0,0 +1,14 @@
+package org.severinu.demoapi.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+
+    @Test
+    void getAll() {
+
+    }
+}
