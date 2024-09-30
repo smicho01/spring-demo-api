e0590c8e0d3fd027faba6d3755e3842708b25282
seweryn.michota <seweryn.michota@digital.homeoffice.gov.uk>
2024-09-30 12:20:44 +0100

    Add logs

--- src/main/java/org/severinu/demoapi/DemoApiApplication.java
+++ src/main/java/org/severinu/demoapi/DemoApiApplication.java
@@ -1,12 +1,16 @@
 package org.severinu.demoapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApiApplication {
 
     public static void main(String[] args) {
+        System.out.println("Demo API Maven app works fine!");
+        log.info("Demo API Maven app works fine!");
         SpringApplication.run(DemoApiApplication.class, args);
     }
 
